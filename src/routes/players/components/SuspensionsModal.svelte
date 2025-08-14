ebac3ac80c260c2499fa3ce9e0aaff3ad256ecbc
Veli Gebrev <vgebrev@gmail.com>
2025-08-14 21:43:05 +0200

    feat: De-clutter players page
     Suspensions list is now in a modal triggered by a button click.

---
+++ src/routes/players/components/SuspensionsModal.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Modal } from 'flowbite-svelte';
+    import SuspensionsList from './SuspensionsList.svelte';
+    import { fade } from 'svelte/transition';
+
+    let { suspendedPlayers = [], open = $bindable(false) } = $props();
+</script>
+
+<Modal
+    transition={fade}
+    bind:open
+    title="Suspensions Watchlist"
+    size="sm"
+    headerClass="p-4 text-md font-semibold"
+    closeBtnClass="p-0">
+    <SuspensionsList {suspendedPlayers} />
+</Modal>
