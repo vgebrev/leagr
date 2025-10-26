66845f705dc8930ee7f56fcc51be17045b9b466f
Veli Gebrev <vgebrev@gmail.com>
2025-10-26 08:22:31 +0200

    fix: Avatar image upload issues in prod (BODY_SIZE_LIMIT)
    Use the BODY_SIZE_LIMIT environmental variable as per the sveltekeit node adapter docs instead of the config Claude hallucinated

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,7 @@ import adapter from '@sveltejs/adapter-node';
 
 const config = {
     kit: {
-        adapter: adapter({
-            bodyParser: {
-                sizeLimit: '6MB' // Allow up to 6MB for avatar uploads (max is 5MB in avatar manager)
-            }
-        }),
+        adapter: adapter(),
         alias: {
             $lib: 'src/lib',
             $components: 'src/components'
